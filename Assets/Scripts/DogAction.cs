ac3272e941f7fc9497165ada6e3f4b317f462e60
Nicolasloure <nicolas.trevizoli2@gmail.com>
2025-12-17 18:28:29 +0100

    Dog + Kid levels for UL

--- Assets/Scripts/DogAction.cs
+++ Assets/Scripts/DogAction.cs
@@ -4,43 +4,67 @@ using UnityEngine;
 public class DogAction : MonoBehaviour
 {
     [SerializeField] Transform endPoint;
+    [SerializeField] Transform awayPoint;
     [SerializeField] float movementSpeed;
 
-    [SerializeField] int waitTime = 5;
+    [SerializeField] int waitTimeToMoveToPlayer = 3;
+    [SerializeField] int waitTimeToMoveAway = 5;
 
     [SerializeField] Collider endCollider;
 
     [SerializeField] AudioSource audioSource;
     [SerializeField] AudioClip barkSound;
 
-    private bool dogMoving = false;
+    private bool dogMovingToPlayer = false;
+    private bool dogMovingAway = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         StartCoroutine (StartMovement());
+        StartCoroutine (MoveAway());
 
     }
 
     // Update is called once per frame
     void Update()
     {
-       MoveDog();
-
+       MoveDogToPlayer();
+       MoveDogAway();
     }
 
     private IEnumerator StartMovement()
     {
-        yield return new WaitForSeconds(waitTime);
-        dogMoving = true;
+        yield return new WaitForSeconds(waitTimeToMoveToPlayer);
+        dogMovingToPlayer = true;
     }
 
-    void MoveDog()
+    private IEnumerator MoveAway()
     {
-        if (dogMoving == true)
+        yield return new WaitForSeconds(waitTimeToMoveAway);
+        dogMovingToPlayer = false;
+        dogMovingAway = true;
+    }
+
+    void MoveDogToPlayer()
+    {
+        if (dogMovingToPlayer == true)
         {
             if(Vector3.Distance(gameObject.transform.position, endPoint.position) > 0.5f) 
             { 
             gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, endPoint.transform.position, movementSpeed * Time.deltaTime);
+            }
+        }
+    }
+
+    void MoveDogAway()
+    {
+        
+        if (dogMovingAway == true)
+        {
+            dogMovingToPlayer = false;
+            if (Vector3.Distance(gameObject.transform.position, awayPoint.position) > 0.5f)
+            {
+                gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, awayPoint.transform.position, movementSpeed * Time.deltaTime);
             }
         }
     }
@@ -53,5 +77,10 @@ public class DogAction : MonoBehaviour
     private void OnTriggerEnter(Collider endCollider)
     {
         Bark();
+    }
+
+    private void OnTriggerExit(Collider endCollider)
+    {
+        audioSource.Stop();
     }
 }
